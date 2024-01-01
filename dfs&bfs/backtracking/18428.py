2e1303c7669c93f73f173804a1cbadc94cbb1540
Kimhyunjin4455 <dp4455@naver.com>
2024-01-02 00:51:13 +0900

    baekJoonQ2225

--- dfs&bfs/backtracking/18428.py
+++ dfs&bfs/backtracking/18428.py
@@ -3,10 +3,10 @@ n = int(input())
 area = [list(input().split()) for _ in range(n)]
 teacher_cnt = 0
 
-# 필요힌 정보
+# 필요한 정보
 dr = [-1,1,0,0]
 dc = [0,0,-1,1]
-for i in range(n):
+for i in range(n): # 선생의 수 파악 위함
     for j in range(n):
         if area[i][j] == 'T':
             teacher_cnt += 1
@@ -14,40 +14,40 @@ result = False
 
 
 def is_check(row, col):
-    for dir in range(4):
+    for dir in range(4): # 상하좌우 탐색
         nr = row + dr[dir]
         nc = col + dc[dir]
-        while 0<=nr<n and 0<=nc<n and area[nr][nc] != 'O':
-            if area[nr][nc] == 'S':
-                return True
-            else: # 학생이 아니라면 그 방향으로 계속 탐색
+        while 0<=nr<n and 0<=nc<n and area[nr][nc] != 'O': # 공간내에서, 장애물이 없는 동안 진행
+            if area[nr][nc] == 'S': # 학생을 만난다면
+                return True # 감시 성공
+            else: # 학생이 아니라면 '그 방향'으로 계속 탐색
                 nr += dr[dir]
                 nc += dc[dir]
-    return False
+    return False # 감시 실패 (그 방향에 학생이 없거나, 장애물에 가려짐)
 
 
 
 def dfs(depth):
     global result
 
-    if depth == 3:
+    if depth == 3: # 3개를 설치했다면
         cnt = 0
         for i in range(n):
             for j in range(n):
-                if area[i][j] == 'T':
-                    if not is_check(i,j): # 감시하지 못하면
+                if area[i][j] == 'T': # 선생을 기준으로
+                    if not is_check(i,j): # 감시하지 못하면 (is_check()는 감시가 불가능 할 때 False 리턴)
                         cnt += 1
-        if cnt == teacher_cnt:
+        if cnt == teacher_cnt: # 전부 다 감시하지 못한다면 (장애물 3개 놓기 성공한다면)
             result = True
         return
 
 
-    for i in range(n):
+    for i in range(n): # 장애물 놓기
         for j in range(n):
             if area[i][j] == 'X':
                 area[i][j] = 'O'
                 dfs(depth+1)
-                area[i][j] = 'X'
+                area[i][j] = 'X' # 원복 처리
 
 dfs(0)
 
---
+++ dp/2225.py
@@ -0,0 +1,18 @@
+n,k = map(int, input().split())
+dp =[[0] * (n+1) for _ in range(k)]
+
+for i in range(k): # k개 이용하여
+    for j in range(n+1): # n만들어가기
+        if i==0: # 인덱스라 0으로 표기했지만 k==1인 경우를 의미
+            dp[i][j] = 1 # n을 1개로 만들때는 n 1가지만 가능
+        elif j==0:
+            dp[i][j] = 1 # 0을 만들때는 0, 0+0, 0+0+0 등 1가지만 가능
+        else:
+            dp[i][j] = (dp[i-1][j] + dp[i][j-1]) % 1000000000 # 점화식
+
+
+print(dp[k-1][n])
+
+
+
+
